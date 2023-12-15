a43ff17724fd7650399c14ae46ea23a9329a9160
Ludwig Prager <ludwig.prager@celp.de>
2023-12-15 20:02:30 +0100

    uses local helm dirs

--- set-env.sh
+++ set-env.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 
 export KUBECONFIG=${BASEDIR:-$(pwd)}/kubeconfig
 
@@ -7,3 +6,7 @@ CLUSTER="opensearch-poc"
 export INGRESS_PORT="8633"
 
 export DNS_DOMAIN=domain.k3d
+
+export HELM_CACHE_HOME=${BASEDIR:-$(pwd)}/.helm/cache
+export HELM_CONFIG_HOME=${BASEDIR:-$(pwd)}/.helm/config
+export HELM_DATA_HOME=${BASEDIR:-$(pwd)}/.helm/data
